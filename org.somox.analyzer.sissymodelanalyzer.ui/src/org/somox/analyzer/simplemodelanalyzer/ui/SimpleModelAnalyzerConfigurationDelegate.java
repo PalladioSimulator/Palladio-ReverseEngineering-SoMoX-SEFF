3d23fb05872cd172533c228c24b198d0d6e4807b
s_burkha <s_burkha>
2013-02-01 14:28:09 +0000

    SOMOX-6 Migration to MoDisco
    org.somox.analyzer.sissymodelanalyzer.ui:
    reenabled GAST2SEFF creation
    
    git-svn-id: https://svnserver.informatik.kit.edu/i43/svn/code/SoMoX/Core/trunk@17930 769cc9a0-fb0c-0410-8c2f-a2305f0e6268

--- org.somox.analyzer.sissymodelanalyzer.ui/src/org/somox/analyzer/simplemodelanalyzer/ui/SimpleModelAnalyzerConfigurationDelegate.java
+++ org.somox.analyzer.sissymodelanalyzer.ui/src/org/somox/analyzer/simplemodelanalyzer/ui/SimpleModelAnalyzerConfigurationDelegate.java
@@ -3,9 +3,13 @@ package org.somox.analyzer.simplemodelanalyzer.ui;
 import java.util.ArrayList;
 
 import org.apache.log4j.Level;
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.IWorkspace;
+import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.debug.core.ILaunch;
 import org.eclipse.debug.core.ILaunchConfiguration;
+import org.eclipse.emf.common.util.URI;
 import org.somox.analyzer.simplemodelanalyzer.jobs.SimpleModelAnalyzerJob;
 import org.somox.configuration.SOMOXConfigurationBuilderByPreferences;
 import org.somox.configuration.SoMoXConfiguration;
@@ -16,6 +20,7 @@ import de.uka.ipd.sdq.workflow.OrderPreservingCompositeJob;
 import de.uka.ipd.sdq.workflow.Workflow;
 import de.uka.ipd.sdq.workflow.launchconfig.AbstractWorkflowBasedLaunchConfigurationDelegate;
 import de.uka.ipd.sdq.workflow.logging.console.LoggerAppenderStruct;
+import eu.qimpress.reverseengineering.gast2seff.jobs.GAST2SEFFJob;
 
 /**
  * The class is defined by the delegate attribute of a launchConfigurationType
@@ -36,11 +41,11 @@ public class SimpleModelAnalyzerConfigurationDelegate
 		// Get the project location.
 		//TODO: fix this if qimpress alternatives are used.
 		//TODO: re-enable GAST2SEFF Job
-//		IWorkspace workspace = ResourcesPlugin.getWorkspace();
-//		IProject project = workspace.getRoot().getProject(config.getSomoxConfiguration().getFileLocations().getProjectName());
+		IWorkspace workspace = ResourcesPlugin.getWorkspace();
+		IProject project = workspace.getRoot().getProject(config.getSomoxConfiguration().getFileLocations().getProjectName());
 //		
 //		TODO: re-enable GAST2SEFF Job
-//		somoxJob.add(new GAST2SEFFJob(URI.createFileURI(project.getFullPath()+"/"+config.getSomoxConfiguration().getFileLocations().getOutputFolder()+"/internal_architecture_model.samm_repository")));
+		somoxJob.add(new GAST2SEFFJob(URI.createFileURI(project.getFullPath()+"/"+config.getSomoxConfiguration().getFileLocations().getOutputFolder()+"/internal_architecture_model.samm_repository")));
 		return somoxJob;
 	}
 
