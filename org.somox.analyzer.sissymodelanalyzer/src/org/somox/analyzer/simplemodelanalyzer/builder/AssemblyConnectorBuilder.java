a43884584f605f0e6c9698d623a2924ad40a36e1
Michael Langhammer <langhammer@kit.edu>
2013-03-27 16:55:03 +0000

    [SOMOX-16] Migrating SoMoX to PCM
    git-svn-id: https://svnserver.informatik.kit.edu/i43/svn/code/SoMoX/Core/trunk@18581 769cc9a0-fb0c-0410-8c2f-a2305f0e6268

--- org.somox.analyzer.sissymodelanalyzer/src/org/somox/analyzer/simplemodelanalyzer/builder/AssemblyConnectorBuilder.java
+++ org.somox.analyzer.sissymodelanalyzer/src/org/somox/analyzer/simplemodelanalyzer/builder/AssemblyConnectorBuilder.java
@@ -1,6 +1,5 @@
 package org.somox.analyzer.simplemodelanalyzer.builder;
 
-import javax.sound.sampled.Port;
 
 import org.apache.log4j.Logger;
 import org.somox.analyzer.AnalysisResult;
@@ -9,16 +8,13 @@ import org.somox.kdmhelper.metamodeladdition.Root;
 
 import de.uka.ipd.sdq.pcm.core.composition.AssemblyConnector;
 import de.uka.ipd.sdq.pcm.core.composition.AssemblyContext;
+import de.uka.ipd.sdq.pcm.core.composition.ComposedStructure;
 import de.uka.ipd.sdq.pcm.core.composition.CompositionFactory;
 import de.uka.ipd.sdq.pcm.core.composition.Connector;
-import de.uka.ipd.sdq.pcm.core.entity.ComposedProvidingRequiringEntity;
 import de.uka.ipd.sdq.pcm.core.entity.NamedElement;
-import de.uka.ipd.sdq.pcm.repository.ComponentType;
-import de.uka.ipd.sdq.pcm.repository.RepositoryComponent;
+import de.uka.ipd.sdq.pcm.repository.OperationProvidedRole;
+import de.uka.ipd.sdq.pcm.repository.OperationRequiredRole;
 //import de.fzi.gast.core.Root;
-import de.uka.ipd.sdq.pcm.repository.RepositoryFactory;
-import de.uka.ipd.sdq.pcm.system.System;
-import de.uka.ipd.sdq.pcm.system.SystemFactory;
 
 /**
  * Builder for Assembly Connectors.
@@ -43,22 +39,19 @@ public class AssemblyConnectorBuilder extends AbstractBuilder {
 	 * @param providedComponentType
 	 */
 	public static Connector createAssemblyConnector(
-			RepositoryComponent parentComponent,
-			Port requiredPort,
-			Port providedPort,
+			ComposedStructure parentComponent,
+			OperationRequiredRole requiredRole,
+			OperationProvidedRole providedRole,
 			AssemblyContext requiredComponentType,
-			RepositoryComponent providedComponentType) {
+			AssemblyContext providedComponentType) {
 		
-		Connector newConnector = createAssemblyConnectorEntity(parentComponent,
-				requiredComponentType, providedComponentType);
-		 
-		SubcomponentEndpoint sourceEndpoint = addSubcomponentEndpoint(
-				newConnector, parentComponent, requiredComponentType);
-		sourceEndpoint.setPort(requiredPort);		
+		AssemblyConnector newConnector = createAssemblyConnectorEntity(parentComponent, 
+						requiredComponentType, providedComponentType);
 		
-		SubcomponentEndpoint targetEndpoint = addSubcomponentEndpoint(
-				newConnector, parentComponent, providedComponentType);
-		targetEndpoint.setPort(providedPort);
+		newConnector.setProvidedRole_AssemblyConnector(providedRole);
+		newConnector.setRequiredRole_AssemblyConnector(requiredRole);
+		newConnector.setProvidingAssemblyContext_AssemblyConnector(providedComponentType);
+		newConnector.setRequiringAssemblyContext_AssemblyConnector(requiredComponentType);
 		
 		return newConnector;
 	}
@@ -72,102 +65,58 @@ public class AssemblyConnectorBuilder extends AbstractBuilder {
 	 * @param requiredComponentInstance
 	 * @param providedComponentInstance
 	 */
-	public static Connector createAssemblyConnector(
-			CompositeStructure parentComponent,
-			Port requiredPort,
-			Port providedPort,
+	/*public static Connector createAssemblyConnector(
+			ComposedStructure parentComponent,
+			OperationRequiredRole requiredPort,
+			OperationProvidedRole providedRole,
 			ComposedProvidingRequiringEntity requiredComponentInstance,
 			ComposedProvidingRequiringEntity providedComponentInstance) {
 
-		Connector newConnector = createAssemblyConnectorEntity(parentComponent,
+		AssemblyConnector newConnector = createAssemblyConnectorEntity(parentComponent,
 				requiredComponentInstance, providedComponentInstance);
-		 
-		SubcomponentEndpoint sourceEndpoint = addSubcomponentEndpoint(
-				newConnector, requiredComponentInstance);
-		sourceEndpoint.setPort(requiredPort);		
 		
-		SubcomponentEndpoint targetEndpoint = addSubcomponentEndpoint(
-				newConnector, providedComponentInstance);
-		targetEndpoint.setPort(providedPort);
+		newConnector.setProvidedRole_AssemblyConnector(providedRole);
+		newConnector.setRequiredRole_AssemblyConnector(requiredPort);
+		newConnector.setProvidingAssemblyContext_AssemblyConnector(providedComponentInstance);
+		newConnector.setRequiringAssemblyContext_AssemblyConnector(requiredComponentInstance);
 		
 		return newConnector;		
-	}
+	}*/
 
-	private static Connector createAssemblyConnectorEntity(
-			ComposedProvidingRequiringEntity parentComponent,
+	private static AssemblyConnector createAssemblyConnectorEntity(
+			ComposedStructure parentComponent,
 			NamedElement requiredComponentType,
-			NamedElement providedComponentType) throws Exception, IllegalAccessException {
+			NamedElement providedComponentType) {
 		logger.debug("Creating new assembly connector from "+requiredComponentType.getEntityName()+" to "+providedComponentType.getEntityName());		
-		Connector newConnector = CompositionFactory.eINSTANCE.createAssemblyConnector();
-		newConnector.setEntityName("");
+		AssemblyConnector newConnector = CompositionFactory.eINSTANCE.createAssemblyConnector();
 		parentComponent.getConnectors__ComposedStructure().add(newConnector);
 		//newConnector.setDocumentation("Assembly Connector from "+requiredComponentType.getEntityName()+" to "+providedComponentType.getEntityName());
 		return newConnector;
 	}	
 
-	/**
-	 * Adds a subcomponent endpoint to an existing connector.
-	 * @param newConnector The connector for which to add a subcomponent endpoint.
-	 * @param subcomponentInstance 
-	 * @return
-	 */
-	private static SubcomponentEndpoint addSubcomponentEndpoint(
-			Connector newConnector, ComposedProvidingRequiringEntity subcomponentInstance) {
-		
-	    SubcomponentEndpoint endpoint = StaticstructureFactory.eINSTANCE.createSubcomponentEndpoint();
-		endpoint.setSubcomponent(subcomponentInstance);
 	
-		newConnector.getEndpoints().add(endpoint);
-		return endpoint;
-	}
-	
-	/**
-	 * Adds a subcomponent endpoint to an existing connector.
-	 * <br>
-	 * Assumption: only one subcomponent instance per component type
-	 * @param newConnector The connector for which to add a subcomponent endpoint.
-	 * @param parentComponent The parent component of the connector
-	 * @param componentTypeOfPort The inner type of the component for which
-	 * to create a subcomponent endpoint. Each type must not occur multiple
-	 * times within the same composite component. 
-	 * @param subcomponentInstance optional; if set to null it will be searched in the 
-	 * parent composite component.
-	 * @return
-	 */
-	private static SubcomponentEndpoint addSubcomponentEndpoint(
-			Connector newConnector, CompositeStructure parentComponent,
-			ComponentType componentTypeOfPort) {
-		
-		SubcomponentEndpoint endpoint = StaticstructureFactory.eINSTANCE.createSubcomponentEndpoint();
-
-		endpoint.setSubcomponent(findComponentInstance(parentComponent, componentTypeOfPort));
-
-		newConnector.getEndpoints().add(endpoint);
-		return endpoint;
-	}	
-
-	public static SubcomponentInstance findComponentInstance(
-			CompositeStructure parentComponent,
-			ComponentType componentTypeOfPort) {
-		SubcomponentInstance resultSubcomponentInstance = null;
-		boolean found = false;
-		for(SubcomponentInstance subcomponentInstance : parentComponent.getSubcomponents()) {
-			
-			if(subcomponentInstance.getRealizedBy().equals(componentTypeOfPort)) {
-				if(found) {
-					throw new IllegalArgumentException("Assumption on input model does not hold. " +
-							"Only one instance per component type per composite component assumed!");
-				}
-				resultSubcomponentInstance = subcomponentInstance;
-				found = true;
-			}
-		}
-		if(!found) {
-			logger.warn("No subcomponent instance found for parent " + parentComponent.getName() +
-					" and child component " + componentTypeOfPort.getName());
-		}
-		return resultSubcomponentInstance;
-		
-	}
+//	public static SubcomponentInstance findComponentInstance(
+//			CompositeStructure parentComponent,
+//			ComponentType componentTypeOfPort) {
+//		SubcomponentInstance resultSubcomponentInstance = null;
+//		boolean found = false;
+//		for(SubcomponentInstance subcomponentInstance : parentComponent.getSubcomponents()) {
+//			
+//			if(subcomponentInstance.getRealizedBy().equals(componentTypeOfPort)) {
+//				if(found) {
+//					throw new IllegalArgumentException("Assumption on input model does not hold. " +
+//							"Only one instance per component type per composite component assumed!");
+//				}
+//				resultSubcomponentInstance = subcomponentInstance;
+//				found = true;
+//			}
+//		}
+//		if(!found) {
+//			logger.warn("No subcomponent instance found for parent " + parentComponent.getName() +
+//					" and child component " + componentTypeOfPort.getName());
+//		}
+//		return resultSubcomponentInstance;
+//		
+//	}
 
 }
