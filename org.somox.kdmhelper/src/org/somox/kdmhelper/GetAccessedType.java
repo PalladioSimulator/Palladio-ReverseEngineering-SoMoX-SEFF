ebb2e3e87dc6da3bbbfaefac623e7fc11f4aad54
s_fschre <s_fschre>
2014-08-14 08:52:04 +0000

    refactoring of kdmreader different handling of internal and external resources registering of jar files fixed proxy resolution
    git-svn-id: https://svnserver.informatik.kit.edu/i43/svn/code/SoMoX/Core/branches/SoMoXWithJaMoPP@23998 769cc9a0-fb0c-0410-8c2f-a2305f0e6268

--- org.somox.kdmhelper/src/org/somox/kdmhelper/GetAccessedType.java
+++ org.somox.kdmhelper/src/org/somox/kdmhelper/GetAccessedType.java
@@ -1,174 +1,32 @@
 package org.somox.kdmhelper;
 
-import java.util.List;
-
-import org.eclipse.emf.ecore.EClass;
-import org.eclipse.emf.ecore.EObject;
-import org.eclipse.jdt.core.dom.EnumConstantDeclaration;
-import org.eclipse.jdt.internal.compiler.ast.ArrayReference;
-import org.emftext.language.java.members.Method;
-import org.emftext.language.java.types.ClassifierReference;
-import org.emftext.language.java.types.NamespaceClassifierReference;
-//import org.eclipse.gmt.modisco.javCommentablede;
-//import org.eclipse.gmt.modisco.java.Method;
-//import org.eclipse.gmt.modisco.java.AbstractTypeDeclaration;
-//import org.eclipse.gmt.modisco.java.AbstractVariablesContainer;
-//import org.eclipse.gmt.modisco.java.ArrayAccess;
-//import org.eclipse.gmt.modisco.java.ClassInstanceCreation;
-//import org.eclipse.gmt.modisco.java.EnumConstantDeclaration;
-//import org.eclipse.gmt.modisco.java.FieldAccess;
-//import org.eclipse.gmt.modisco.java.FieldDeclaration;
-//import org.eclipse.gmt.modisco.java.SingleVariableAccess;
-//import org.eclipse.gmt.modisco.java.SingleVariableDeclaration;
-//import org.eclipse.gmt.modisco.java.SuperFieldAccess;
-//import org.eclipse.gmt.modisco.java.ThisExpression;
-//import org.eclipse.gmt.modisco.java.Type;
-//import org.eclipse.gmt.modisco.java.TypeAccess;
-//import org.eclipse.gmt.modisco.java.emf.util.JavaSwitch;
+import org.emftext.language.java.commons.Commentable;
 import org.emftext.language.java.types.Type;
 import org.emftext.language.java.types.TypeReference;
-import org.emftext.language.java.util.JavaSwitch;
-import org.emftext.language.java.*;
-import org.emftext.language.java.commons.Commentable;
-import org.emftext.language.java.containers.CompilationUnit;
 
-/**
- * A EMF-Switch to get the accessed type of an access.
- * @author Oliver
- *
- */
-public class GetAccessedType extends JavaSwitch<Type> {
-
-	
-	/**
-	 * Singleton.
-	 */
-	private static GetAccessedType getInstance = new GetAccessedType();
+public class GetAccessedType {
 
 	/**
 	 * Computes the accessed type for an access.
-	 * @param input The input access.
+	 * 
+	 * @param input
+	 *            The input access.
 	 * @return The accessed Type from the access.
 	 */
 	public static Type getAccessedType(Commentable input) {
-		if (input instanceof NamespaceClassifierReference) {
-			return getAccessedType((NamespaceClassifierReference) input);
+		if (input instanceof TypeReference) {
+			return getAccessedType((TypeReference) input);
+		} else {
+			return null;
 		}
-		return getInstance.doSwitch(input);
 	}
-
-	private static Type getAccessedType(NamespaceClassifierReference reference) {
-		return null;
+	
+	public static Type getAccessedType(TypeReference reference) {
+		if (reference != null) {
+			return reference.getTarget();
+		} else {
+			return null;
+		}
 	}
-
-	//TODO Override the methods in JavaSwitch
-//	@Override
-//	public Type caseAbstractMethodInvocation(Method object) {
-//		if (object instanceof ClassInstanceCreation) {
-//			return ((ClassInstanceCreation) object).getType().getType();
-//		}
-//		return object.getMethod().getAbstractTypeDeclaration();
-//	}
-//
-//	
-//	
-//	@Override
-//	public Type caseTypeAccess(TypeReference        object) {
-//		return object.getType ();
-//	}
-//
-//	@Override
-//	public Type caseArrayAccess(ArrayReference object) {
-//		return getInstance.doSwitch(object.getArray());
-//	}
-//
-//	@Override
-//	public Type caseFieldAccess(FieldAccess object) {
-//		return getInstance.doSwitch(object.getField());
-//		// return object.getField().getQualifier().get
-//	}
-
-	/**
-	 * Used to compute in which type a {@link ASTNode} is placed.
-	 * @param input
-	 * @return
-	 */
-//	private static Type getAbstractTypeDeclarationContainer(Commentable input) {
-//		Commentable node = input;
-//		while (!(node.eContainer() instanceof AbstractTypeDeclaration)) {
-//			node = (Commentable) node.eContainer();
-//		}
-//		return (Type) node.eContainer();
-//	}
-
-//	@Override
-//	public Type caseSingleVariableAccess(SingleVariableAccess object) {
-//
-//		// ASTNode node = object;
-//		// while (!(node.eContainer() instanceof AbstractTypeDeclaration)) {
-//		// node = (ASTNode) node.eContainer();
-//		// }
-//		// ASTNode tempResult1 = (Type) (node.eContainer());
-//
-//		if (object.getVariable() != null) {
-//			// if (object.getVariable() instanceof SingleVariableDeclaration
-//			// | object.getVariable() instanceof EnumConstantDeclaration) {
-//			// return getAbstractTypeDeclaration(object.getVariable());
-//			// }
-//			return getAbstractTypeDeclarationContainer(object.getVariable());
-//
-//		}
-////		System.out.println("VDF");
-//		return null;
-//		// ASTNode tempResult2 =
-//		// getInstance.doSwitch(object.getVariable().eContainer());
-//
-//		// alt
-//		// if(object.getVariable() != null){
-//		// if (object.getVariable() instanceof SingleVariableDeclaration |
-//		// object.getVariable() instanceof EnumConstantDeclaration){
-//		// return getInstance.doSwitch(object.getVariable());
-//		// }
-//		//
-//		// }
-//		// return getInstance.doSwitch(object.getVariable().eContainer());
-//	}
-
-//	@Override
-//	public Type caseEnumConstantDeclaration(EnumConstantDeclaration object) {
-//		return (Type) object.eContainer();
-//	}
-//
-//	@Override
-//	public Type caseSingleVariableDeclaration(SingleVariableDeclaration object) {
-//		return getInstance.doSwitch(object.getType());
-//	}
-//
-//	@Override
-//	public Type caseAbstractVariablesContainer(AbstractVariablesContainer object) {
-//		return object.getType().getType();
-//	}
-//
-//	@Override
-//	public Type caseFieldDeclaration(FieldDeclaration object) {
-//		return object.getAbstractTypeDeclaration();
-//	}
-//
-//	@Override
-//	public Type caseSuperFieldAccess(SuperFieldAccess object) {
-//		return new GetAccessedType().doSwitch(object.getField());
-//	}
-//
-//	@Override
-//	public Type caseThisExpression(ThisExpression object) {
-//		EObject container = object.eContainer();
-//		while (container != null) {
-//			if (container instanceof Type) {
-//				return (Type) container;
-//			}
-//			container = container.eContainer();
-//		}
-//		throw new IllegalArgumentException();
-//	}
 
 }
--- org.somox.kdmhelper/src/org/somox/kdmhelper/KDMReader.java
+++ org.somox.kdmhelper/src/org/somox/kdmhelper/KDMReader.java
@@ -5,6 +5,8 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashMap;
+import java.util.Iterator;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
@@ -13,8 +15,10 @@ import org.apache.commons.io.FileUtils;
 import org.apache.log4j.Logger;
 import org.eclipse.core.resources.IProject;
 import org.eclipse.core.runtime.CoreException;
+import org.eclipse.emf.common.notify.Notifier;
 import org.eclipse.emf.common.util.URI;
 import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.InternalEObject;
 import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.emf.ecore.resource.ResourceSet;
 import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
@@ -25,17 +29,15 @@ import org.eclipse.jdt.core.IJavaProject;
 import org.eclipse.jdt.core.IPackageFragmentRoot;
 import org.eclipse.jdt.core.JavaCore;
 import org.eclipse.jdt.core.JavaModelException;
+import org.emftext.language.java.JavaClasspath;
 import org.emftext.language.java.containers.CompilationUnit;
-import org.emftext.language.java.containers.JavaRoot;
 import org.emftext.language.java.resource.java.util.JavaResourceUtil;
 import org.somox.kdmhelper.metamodeladdition.Root;
-
 
 public class KDMReader {
 
 	private Root root;
-	//Resource 
-
+	// Resource
 
 	private final static Logger logger = Logger.getLogger(KDMReader.class.getName());
 
@@ -46,7 +48,7 @@ public class KDMReader {
 	public Root getRoot() {
 		return root;
 	}
-	
+
 	public void loadProject(IProject project) throws IOException {
 		try {
 			if (!project.isNatureEnabled("org.eclipse.jdt.core.javanature")) {
@@ -56,47 +58,120 @@ public class KDMReader {
 			throw new IOException("Checking nature of project failed.", e);
 		}
 		IJavaProject javaProject = JavaCore.create(project);
-		List<IPackageFragmentRoot> sourceFolders = new ArrayList<IPackageFragmentRoot>();
+		loadJavaProject(javaProject);
+	}
+	
+	private void loadJavaProject(IJavaProject javaProject) throws IOException {
+		ResourceSet rs = new ResourceSetImpl();
 		try {
 			IPackageFragmentRoot[] packageFragmentRoots = javaProject.getAllPackageFragmentRoots();
 			for (IPackageFragmentRoot pfr : packageFragmentRoots) {
+				String path = pfr.getPath().toString();
+				if (!pfr.isExternal()) {
+					path = javaProject.getProject().getLocation().toString() + "/.." + path;
+				}
 				if (pfr.getKind() == IPackageFragmentRoot.K_SOURCE) {
-					sourceFolders.add(pfr);
+					loadSourceFolder(path, rs);
+				} else if (pfr.getKind() == IPackageFragmentRoot.K_BINARY) {
+					registerJarFile(path, rs);
 				}
 			}
 		} catch (JavaModelException e1) {
 			e1.printStackTrace();
 		}
-
-		ResourceSet rs = new ResourceSetImpl();
-		for (IPackageFragmentRoot src : sourceFolders) {
-			String path = project.getLocation().toString() + "/.." + src.getPath().toString();
-			File directory = new File(path);
-	        Collection<File> javaFiles = FileUtils.listFiles(directory, new String[] { "java" }, true);
-	        for (File javaFile : javaFiles) {
-	            Resource resource = null;
-				try {
-					resource = rs.getResource(URI.createFileURI(javaFile.getCanonicalPath()), true);
-					// TODO fix: execution of following statement leads to ClassCastException
-					// Map<Object, Object> loadOptions = setupLoadOptions(resource);
-					resource.load(null);
-					boolean resolvedAll = JavaResourceUtil.resolveAll(resource);
-					if(!resolvedAll){
-						logger.info("Could not resolve all proxies in resource " + resource.getURI());
-						Set<EObject> proxies = JavaResourceUtil.findUnresolvedProxies(resource);
-						List<String> identifiers = new ArrayList<String>();
-						for (EObject proxy : proxies) {
-							identifiers.add(JavaResourceUtil.getProxyIdentifier(proxy));
-						}
-						logger.info("Unresolved proxies: " + identifiers);
-					}
-					addModelToRoot(resource);
-				} catch (IOException e) {
-					e.printStackTrace();
-	                logger.warn("Failed to load resource: " + javaFile + "\n Reason: " + e);
-				}
-	        }
+		if (!resolveAllProxies(rs)) {
+			logger.error("Resolution of some Proxies failed...");
 		}
+	}
+	
+	private void loadSourceFolder(String absolutePath, ResourceSet rs) throws IOException {
+		File directory = new File(absolutePath);
+		if (!directory.isDirectory()) {
+			throw new IOException("Given path is not a directory.");
+		}
+		loadSourceFolder(directory, rs);
+	}
+		
+	private void loadSourceFolder(File directory, ResourceSet rs) {
+		Collection<File> javaFiles = FileUtils.listFiles(directory,	new String[] { "java" }, true);
+		for (File javaFile : javaFiles) {
+			Resource resource = null;
+			try {
+				resource = rs.getResource(URI.createFileURI(javaFile.getCanonicalPath()), true);
+				// TODO fix: execution of following statement leads to ClassCastException
+				// Map<Object, Object> loadOptions = setupLoadOptions(resource);
+				logger.info("Parsing resource: " + javaFile);
+				resource.load(null);
+				addModelToRoot(resource);
+			} catch (IOException e) {
+				e.printStackTrace();
+				logger.warn("Failed to load resource: " + javaFile + "\n Reason: " + e);
+			}
+		}
+	}
+	
+	private void registerJarFile(String jarPath, ResourceSet rs) throws IOException {
+		File jar = new File(jarPath);
+		registerJarFile(jar, rs);
+	}
+	
+	private void registerJarFile(File jarPath, ResourceSet rs) throws IOException {
+		JavaClasspath cp = JavaClasspath.get(rs);
+		logger.info("Registering JAR " + jarPath.getCanonicalPath());
+		cp.registerClassifierJar(URI.createFileURI(jarPath.getCanonicalPath()));
+	}
+	
+	private boolean resolveAllProxies(ResourceSet rs) {
+		return resolveAllProxies(0, rs);
+	}
+	
+	private boolean resolveAllProxies(int resourcesProcessedBefore, ResourceSet rs) {
+		boolean failure = false;
+		List<EObject> eobjects = new LinkedList<EObject>();
+		for (Iterator<Notifier> i = rs.getAllContents(); i.hasNext();) {
+			Notifier next = i.next();
+			if (next instanceof EObject) {
+				eobjects.add((EObject) next);
+			}
+		}
+		int resourcesProcessed = rs.getResources().size();
+		if (resourcesProcessed == resourcesProcessedBefore) {
+			return true;
+		}
+
+		logger.info("Resolving cross-references of " + eobjects.size() + " EObjects.");
+		int resolved = 0;
+		int notResolved = 0;
+		int eobjectCnt = 0;
+		for (EObject next : eobjects) {
+			eobjectCnt++;
+			if (eobjectCnt % 1000 == 0) {
+				logger.info(eobjectCnt + "/" + eobjects.size()
+						+ " done: Resolved " + resolved + " crossrefs, "
+						+ notResolved + " crossrefs could not be resolved.");
+			}
+
+			InternalEObject nextElement = (InternalEObject) next;
+			for (EObject crElement : nextElement.eCrossReferences()) {
+				crElement = EcoreUtil.resolve(crElement, rs);
+				if (crElement.eIsProxy()) {
+					failure = true;
+					notResolved++;
+					logger.info("Can not find referenced element in classpath: "
+									+ ((InternalEObject) crElement).eProxyURI());
+				} else {
+					resolved++;
+				}
+			}
+		}
+
+		logger.info(eobjectCnt + "/" + eobjects.size()
+				+ " done: Resolved " + resolved + " crossrefs, " + notResolved
+				+ " crossrefs could not be resolved.");
+		
+		//call this method again, because the resolving might have triggered loading
+		//of additional resources that may also contain references that need to be resolved.
+		return !failure && resolveAllProxies(resourcesProcessed, rs);
 	}
 
 	public void loadFiles(Collection<String> filesLocationList)
@@ -105,10 +180,10 @@ public class KDMReader {
 			File file = new File(fileLocation);
 			URI URIfile = URI.createFileURI(file.getAbsolutePath());
 
-			//Resource resource = ModelUtils.loadModel(URIfile);
-			
-			ResourceSet rs= new ResourceSetImpl();
-			Resource resource = rs.getResource(URIfile,true);
+			// Resource resource = ModelUtils.loadModel(URIfile);
+
+			ResourceSet rs = new ResourceSetImpl();
+			Resource resource = rs.getResource(URIfile, true);
 			Map<Object, Object> loadOptions = setupLoadOptions(resource);
 			resource.load(null);
 			addModelToRoot(resource);
@@ -118,11 +193,11 @@ public class KDMReader {
 
 	// TODO test
 	public void loadFile(URI file) throws IOException {
-		
-		ResourceSet rs= new ResourceSetImpl();
-		Resource resource = rs.getResource(file,true);
-		
-//		resource = ModelUtils.loadModel(file);
+
+		ResourceSet rs = new ResourceSetImpl();
+		Resource resource = rs.getResource(file, true);
+
+		// resource = ModelUtils.loadModel(file);
 		addModelToRoot(resource);
 
 	}
@@ -130,10 +205,10 @@ public class KDMReader {
 	private void addModelToRoot(Resource resource) {
 		root.addModels(getModelsFromResource(resource));
 	}
-	
-	// CompilationUnit statt Model 
+
+	// CompilationUnit statt Model
 	// JavaRoot statt JavaAplication
-	private Collection<CompilationUnit> getModelsFromResource(Resource resource) { 
+	private Collection<CompilationUnit> getModelsFromResource(Resource resource) {
 		List<CompilationUnit> modelList = new ArrayList<CompilationUnit>();
 		for (EObject obj : resource.getContents()) {
 			if (obj instanceof CompilationUnit) {
@@ -145,17 +220,12 @@ public class KDMReader {
 	}
 
 	private Map<Object, Object> setupLoadOptions(Resource resource) {
-		Map<Object, Object> loadOptions = ((XMLResource) resource)
-				.getDefaultLoadOptions();
+		Map<Object, Object> loadOptions = ((XMLResource) resource).getDefaultLoadOptions();
 		loadOptions.put(XMLResource.OPTION_DEFER_ATTACHMENT, Boolean.TRUE);
-		loadOptions
-				.put(XMLResource.OPTION_DEFER_IDREF_RESOLUTION, Boolean.TRUE);
-		loadOptions
-				.put(XMLResource.OPTION_USE_DEPRECATED_METHODS, Boolean.TRUE);
-		loadOptions.put(XMLResource.OPTION_USE_PARSER_POOL,
-				new XMLParserPoolImpl());
-		loadOptions.put(XMLResource.OPTION_USE_XML_NAME_TO_FEATURE_MAP,
-				new HashMap<Object, Object>());
+		loadOptions.put(XMLResource.OPTION_DEFER_IDREF_RESOLUTION, Boolean.TRUE);
+		loadOptions.put(XMLResource.OPTION_USE_DEPRECATED_METHODS, Boolean.TRUE);
+		loadOptions.put(XMLResource.OPTION_USE_PARSER_POOL, new XMLParserPoolImpl());
+		loadOptions.put(XMLResource.OPTION_USE_XML_NAME_TO_FEATURE_MAP, new HashMap<Object, Object>());
 		return loadOptions;
 	}
 
