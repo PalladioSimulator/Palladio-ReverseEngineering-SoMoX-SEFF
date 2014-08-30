c884db3d045993ecd80267c602fe828ecc38fa3b
s_fschre <s_fschre>
2014-08-30 16:34:20 +0000

    copy to branch
    git-svn-id: https://svnserver.informatik.kit.edu/i43/svn/code/SoMoX/Core/branches/SoMoXWithJaMoPP@24233 769cc9a0-fb0c-0410-8c2f-a2305f0e6268

---
+++ org.somox.metrics.dsl.tests/src/org/somox/metrics/dsl/tests/MetricDSLTest.java
@@ -0,0 +1,14 @@
+package org.somox.metrics.dsl.tests;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class MetricDSLTest {
+
+    @Test
+    public void test() {
+        assertTrue(true);
+    }
+
+}
