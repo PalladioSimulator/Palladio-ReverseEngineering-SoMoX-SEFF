eae4910c0be8061021b72cdb3fd2ac2f3a7f83bb
Michael Langhammer <langhammer@kit.edu>
2015-05-10 11:07:17 +0000

    some fixes to avoid a lot of loggers
    git-svn-id: https://svnserver.informatik.kit.edu/i43/svn/code/SoMoX/Core/branches/SoMoXWithJaMoPP@26461 769cc9a0-fb0c-0410-8c2f-a2305f0e6268

--- org.somox.analyzer.sissymodelanalyzer/src/org/somox/analyzer/simplemodelanalyzer/Activator.java
+++ org.somox.analyzer.sissymodelanalyzer/src/org/somox/analyzer/simplemodelanalyzer/Activator.java
@@ -1,8 +1,5 @@
 package org.somox.analyzer.simplemodelanalyzer;
 
-import org.apache.log4j.ConsoleAppender;
-import org.apache.log4j.Logger;
-import org.apache.log4j.PatternLayout;
 import org.eclipse.core.runtime.Plugin;
 import org.eclipse.core.runtime.Status;
 import org.osgi.framework.BundleContext;
@@ -12,53 +9,57 @@ import org.osgi.framework.BundleContext;
  */
 public class Activator extends Plugin {
 
-	// The plug-in ID
-	public static final String PLUGIN_ID = "org.somox.analyzer.simplemodelanalyzer";
-	
-	// The shared instance
-	/**
-	 * @uml.property  name="plugin"
-	 * @uml.associationEnd  
-	 */
-	private static Activator plugin;
+    // The plug-in ID
+    public static final String PLUGIN_ID = "org.somox.analyzer.simplemodelanalyzer";
 
-	/**
-	 * The constructor
-	 */
-	public Activator() {
-	}
+    // The shared instance
+    /**
+     * @uml.property name="plugin"
+     * @uml.associationEnd
+     */
+    private static Activator plugin;
 
-	/*
-	 * (non-Javadoc)
-	 * @see org.eclipse.core.runtime.Plugins#start(org.osgi.framework.BundleContext)
-	 */
-	public void start(BundleContext context) throws Exception {
-		super.start(context);
-		plugin = this;		
-		getLog().log(new Status(Status.INFO,Activator.PLUGIN_ID,"Analyzer Activator called"));
+    /**
+     * The constructor
+     */
+    public Activator() {
+    }
 
-		//debug:
-		Logger rootLogger = Logger.getRootLogger();
-		rootLogger.addAppender(new ConsoleAppender(
-				 new PatternLayout(PatternLayout.TTCC_CONVERSION_PATTERN)));
-	}
+    /*
+     * (non-Javadoc)
+     * 
+     * @see org.eclipse.core.runtime.Plugins#start(org.osgi.framework.BundleContext)
+     */
+    @Override
+    public void start(final BundleContext context) throws Exception {
+        super.start(context);
+        plugin = this;
+        this.getLog().log(new Status(Status.INFO, Activator.PLUGIN_ID, "Analyzer Activator called"));
 
-	/*
-	 * (non-Javadoc)
-	 * @see org.eclipse.core.runtime.Plugin#stop(org.osgi.framework.BundleContext)
-	 */
-	public void stop(BundleContext context) throws Exception {
-		plugin = null;
-		super.stop(context);
-	}
+        // debug:
+        // Logger rootLogger = Logger.getRootLogger();
+        // rootLogger.addAppender(new ConsoleAppender(
+        // new PatternLayout(PatternLayout.TTCC_CONVERSION_PATTERN)));
+    }
 
-	/**
-	 * Returns the shared instance
-	 *
-	 * @return the shared instance
-	 */
-	public static Activator getDefault() {
-		return plugin;
-	}
+    /*
+     * (non-Javadoc)
+     * 
+     * @see org.eclipse.core.runtime.Plugin#stop(org.osgi.framework.BundleContext)
+     */
+    @Override
+    public void stop(final BundleContext context) throws Exception {
+        plugin = null;
+        super.stop(context);
+    }
+
+    /**
+     * Returns the shared instance
+     *
+     * @return the shared instance
+     */
+    public static Activator getDefault() {
+        return plugin;
+    }
 
 }
--- org.somox.gast2seff/src/org/somox/gast2seff/Activator.java
+++ org.somox.gast2seff/src/org/somox/gast2seff/Activator.java
@@ -1,9 +1,5 @@
 package org.somox.gast2seff;
 
-import org.apache.log4j.ConsoleAppender;
-import org.apache.log4j.Level;
-import org.apache.log4j.Logger;
-import org.apache.log4j.PatternLayout;
 import org.eclipse.jface.resource.ImageDescriptor;
 import org.eclipse.ui.plugin.AbstractUIPlugin;
 import org.osgi.framework.BundleContext;
@@ -13,61 +9,65 @@ import org.osgi.framework.BundleContext;
  */
 public class Activator extends AbstractUIPlugin {
 
-	// The plug-in ID
-	public static final String PLUGIN_ID = "org.somox.gast2seff";
+    // The plug-in ID
+    public static final String PLUGIN_ID = "org.somox.gast2seff";
 
-	// The shared instance
-	private static Activator plugin;
-	
-	/**
-	 * The constructor
-	 */
-	public Activator() {
-	}
+    // The shared instance
+    private static Activator plugin;
 
-	/*
-	 * (non-Javadoc)
-	 * @see org.eclipse.ui.plugin.AbstractUIPlugin#start(org.osgi.framework.BundleContext)
-	 */
-	public void start(BundleContext context) throws Exception {
-		super.start(context);
-		plugin = this;
-		
-		//debug:
-		Logger rootLogger = Logger.getRootLogger();
-        if (!rootLogger.getAllAppenders().hasMoreElements()) {
-    		rootLogger.addAppender(new ConsoleAppender(
-    				 new PatternLayout(PatternLayout.TTCC_CONVERSION_PATTERN)));	
-    		rootLogger.setLevel(Level.ALL);
-        }
-	}
+    /**
+     * The constructor
+     */
+    public Activator() {
+    }
 
-	/*
-	 * (non-Javadoc)
-	 * @see org.eclipse.ui.plugin.AbstractUIPlugin#stop(org.osgi.framework.BundleContext)
-	 */
-	public void stop(BundleContext context) throws Exception {
-		plugin = null;
-		super.stop(context);
-	}
+    /*
+     * (non-Javadoc)
+     * 
+     * @see org.eclipse.ui.plugin.AbstractUIPlugin#start(org.osgi.framework.BundleContext)
+     */
+    @Override
+    public void start(final BundleContext context) throws Exception {
+        super.start(context);
+        plugin = this;
 
-	/**
-	 * Returns the shared instance
-	 *
-	 * @return the shared instance
-	 */
-	public static Activator getDefault() {
-		return plugin;
-	}
+        // debug:
+        // Logger rootLogger = Logger.getRootLogger();
+        // if (!rootLogger.getAllAppenders().hasMoreElements()) {
+        // rootLogger.addAppender(new ConsoleAppender(
+        // new PatternLayout(PatternLayout.TTCC_CONVERSION_PATTERN)));
+        // rootLogger.setLevel(Level.ALL);
+        // }
+    }
 
-	/**
-	 * Returns an image descriptor for the image file at the given
-	 * plug-in relative path
-	 *
-	 * @param path the path
-	 * @return the image descriptor
-	 */
-	public static ImageDescriptor getImageDescriptor(String path) {
-		return imageDescriptorFromPlugin(PLUGIN_ID, path);
-	}
+    /*
+     * (non-Javadoc)
+     * 
+     * @see org.eclipse.ui.plugin.AbstractUIPlugin#stop(org.osgi.framework.BundleContext)
+     */
+    @Override
+    public void stop(final BundleContext context) throws Exception {
+        plugin = null;
+        super.stop(context);
+    }
+
+    /**
+     * Returns the shared instance
+     *
+     * @return the shared instance
+     */
+    public static Activator getDefault() {
+        return plugin;
+    }
+
+    /**
+     * Returns an image descriptor for the image file at the given plug-in relative path
+     *
+     * @param path
+     *            the path
+     * @return the image descriptor
+     */
+    public static ImageDescriptor getImageDescriptor(final String path) {
+        return imageDescriptorFromPlugin(PLUGIN_ID, path);
+    }
 }
