475ffedc30a4fc36008cd3ed007e648a1406519b
Michael Langhammer <langhammer@kit.edu>
2015-12-07 09:30:16 +0000

    + improved VisitorUtils.findMethods + new tests for find methods
    git-svn-id: https://svnserver.informatik.kit.edu/i43/svn/code/SoMoX/Core/branches/SoMoXWithJaMoPP@30175 769cc9a0-fb0c-0410-8c2f-a2305f0e6268

--- org.somox.gast2seff/src/org/somox/gast2seff/visitors/VisitorUtils.java
+++ org.somox.gast2seff/src/org/somox/gast2seff/visitors/VisitorUtils.java
@@ -1,17 +1,19 @@
 package org.somox.gast2seff.visitors;
 
-import java.util.ArrayList;
-import java.util.Collections;
+import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Set;
 
 import org.apache.log4j.Logger;
 import org.eclipse.emf.common.util.EList;
+import org.eclipse.emf.common.util.TreeIterator;
 import org.eclipse.emf.ecore.EObject;
 import org.emftext.language.java.annotations.AnnotationAttribute;
 import org.emftext.language.java.expressions.Expression;
 import org.emftext.language.java.members.Method;
 import org.emftext.language.java.references.MethodCall;
+import org.emftext.language.java.references.Reference;
 import org.emftext.language.java.statements.Statement;
 import org.emftext.language.java.statements.StatementListContainer;
 import org.palladiosimulator.pcm.repository.BasicComponent;
@@ -36,54 +38,58 @@ public class VisitorUtils {
      */
     public static List<Method> getMethodCalls(final Statement statement) {
         final LinkedList<Method> calledMethods = new LinkedList<Method>();
+        final Set<EObject> investigatedEObjects = new HashSet<EObject>();
 
-        final List<MethodCall> methodCalls = findMethodCalls(statement);
+        findMethodCallsInChildren(statement, calledMethods, investigatedEObjects);
 
-        if (null != methodCalls) {
-            for (final MethodCall methodCall : methodCalls) {
-                if (methodCall.getTarget() instanceof AnnotationAttribute) {
-                    logger.info("Annotation Attribut found within statement " + statement);
-                }
-                if (methodCall.getTarget() instanceof Method) {
-                    calledMethods.add((Method) methodCall.getTarget());
-                }
-            }
-        }
-        final EList<Method> childMethodsOfStatement = statement.getChildrenByType(Method.class);
-        if (null != childMethodsOfStatement && 0 < childMethodsOfStatement.size()) {
-            logger.info("Add " + childMethodsOfStatement.size() + " childMethods to calledMethod list");
-            calledMethods.addAll(childMethodsOfStatement);
-        }
         return calledMethods;
-
     }
 
-    private static List<MethodCall> findMethodCalls(final EObject parentEObject) {
-        if (null == parentEObject || null == parentEObject.eContents()) {
-            return Collections.emptyList();
+    private static void findMethodCallsInChildren(final EObject eObject, final LinkedList<Method> calledMethods,
+            final Set<EObject> investigatedEObjects) {
+        if (null == eObject) {
+            return;
         }
-        final List<MethodCall> methodCalls = new ArrayList<MethodCall>();
-        if (parentEObject instanceof MethodCall) {
-            handleMethodCall(methodCalls, (MethodCall) parentEObject);
-        }
-        for (final EObject eObject : parentEObject.eContents()) {
-            if (eObject instanceof MethodCall) {
-                handleMethodCall(methodCalls, (MethodCall) eObject);
-            } else {
-                methodCalls.addAll(findMethodCalls(eObject));
+        final TreeIterator<EObject> treeIterator = eObject.eAllContents();
+        while (treeIterator.hasNext()) {
+            final EObject current = treeIterator.next();
+            if (investigatedEObjects.contains(current)) {
+                continue;
+            }
+            investigatedEObjects.add(current);
+            if (current instanceof MethodCall) {
+                final MethodCall methodCall = (MethodCall) current;
+                findMethodCallsInArguments(methodCall.getArguments(), calledMethods, investigatedEObjects);
+                addMethodToCollection(calledMethods, methodCall);
+                findMethodCallsInNext(methodCall.getNext(), calledMethods, investigatedEObjects);
             }
         }
-        return methodCalls;
+    }
+
+    private static void findMethodCallsInNext(final Reference next, final LinkedList<Method> calledMethods,
+            final Set<EObject> investigatedEObjects) {
+        findMethodCallsInChildren(next, calledMethods, investigatedEObjects);
 
     }
 
-    private static void handleMethodCall(final List<MethodCall> methodCalls, final MethodCall methodCall) {
-        for (int i = methodCall.getArguments().size() - 1; i >= 0; i--) {
-            final Expression expression = methodCall.getArguments().get(i);
-            methodCalls.addAll(findMethodCalls(expression));
+    private static void findMethodCallsInArguments(final EList<Expression> arguments,
+            final LinkedList<Method> calledMethods, final Set<EObject> investigatedEObjects) {
+        for (final Expression expression : arguments) {
+            findMethodCallsInChildren(expression, calledMethods, investigatedEObjects);
         }
-        methodCalls.add(methodCall);
-        methodCalls.addAll(findMethodCalls(methodCall.getNext()));
+
+    }
+
+    private static void addMethodToCollection(final LinkedList<Method> calledMethods, final MethodCall methodCall) {
+        if (methodCall.getTarget() instanceof Method) {
+            final Method target = (Method) methodCall.getTarget();
+            if (target instanceof AnnotationAttribute) {
+                logger.info("Annotation Attribut found within methodCall" + methodCall + " target: "
+                        + methodCall.getTarget().getName());
+            } else if (target instanceof Method) {
+                calledMethods.add(target);
+            }
+        }
     }
 
     public static final void visitJaMoPPMethod(final ResourceDemandingBehaviour seff,
