00ba97a904a7f42103e9db36f2d0749e6c876ca4
Michael Langhammer <langhammer@kit.edu>
2016-02-23 10:43:49 +0000

    use platform URI instead of pathmap resource if possible
    git-svn-id: https://svnserver.informatik.kit.edu/i43/svn/code/SoMoX/Core/branches/SoMoXWithJaMoPP@31157 769cc9a0-fb0c-0410-8c2f-a2305f0e6268

--- org.somox.kdmhelper/src/org/splevo/jamopp/extraction/SPLevoResourceSet.java
+++ org.somox.kdmhelper/src/org/splevo/jamopp/extraction/SPLevoResourceSet.java
@@ -20,6 +20,7 @@ import org.eclipse.core.runtime.Path;
 import org.eclipse.emf.common.util.URI;
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.resource.Resource;
+import org.eclipse.emf.ecore.resource.URIConverter;
 import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
 
 import com.google.common.base.Strings;
@@ -31,41 +32,48 @@ import com.google.common.base.Strings;
  */
 public class SPLevoResourceSet extends ResourceSetImpl {
 
-    
-    
     @Override
-    public EObject getEObject(URI uri, boolean loadOnDemand) {
-        return super.getEObject(convertURI(uri), loadOnDemand);
+    public EObject getEObject(final URI uri, final boolean loadOnDemand) {
+        return super.getEObject(this.convertURI(uri), loadOnDemand);
     }
 
     @Override
-    public Resource getResource(URI uri, boolean loadOnDemand) {
-        return super.getResource(convertURI(uri), loadOnDemand);
+    public Resource getResource(final URI uri, final boolean loadOnDemand) {
+        return super.getResource(this.convertURI(uri), loadOnDemand);
     }
 
     @Override
-    public Resource createResource(URI uri, String contentType) {
-        return super.createResource(convertURI(uri), contentType);
+    public Resource createResource(final URI uri, final String contentType) {
+        return super.createResource(this.convertURI(uri), contentType);
     }
 
-    private URI convertURI(URI uri) {
+    private URI convertURI(final URI uri) {
 
         if (FileResourceHandling.usePlatformResource(this)) {
 
-            if (uri.isPlatform() || "pathmap".equals(uri.scheme())) {
+            if (uri.isPlatform()) {
+                return uri;
+            } else if ("pathmap".equals(uri.scheme())) {
+                // avoid pathmap if possible:
+                final URIConverter theURIConverter = this.getURIConverter();
+                final URI normalizedURI = theURIConverter.normalize(uri);
+                if (normalizedURI.isPlatform()) {
+                    return normalizedURI;
+                }
                 return uri;
             } else {
-                IWorkspaceRoot root = ResourcesPlugin.getWorkspace().getRoot();
-                IPath p = new Path(uri.toFileString());
-                IFile f = root.getFileForLocation(p);
-                return createURI(URI.createPlatformResourceURI(f.getFullPath().toString(), true), uri.fragment());
+                final IWorkspaceRoot root = ResourcesPlugin.getWorkspace().getRoot();
+                final IPath p = new Path(uri.toFileString());
+                final IFile f = root.getFileForLocation(p);
+                return SPLevoResourceSet.createURI(URI.createPlatformResourceURI(f.getFullPath().toString(), true),
+                        uri.fragment());
             }
 
         } else {
 
             if (uri.isPlatform()) {
-                IWorkspaceRoot root = ResourcesPlugin.getWorkspace().getRoot();
-                IResource f = root.findMember(uri.toPlatformString(true));
+                final IWorkspaceRoot root = ResourcesPlugin.getWorkspace().getRoot();
+                final IResource f = root.findMember(uri.toPlatformString(true));
                 return URI.createFileURI(f.getLocationURI().toString());
             } else {
                 return uri;
@@ -74,8 +82,8 @@ public class SPLevoResourceSet extends ResourceSetImpl {
         }
 
     }
-        
-    private static URI createURI(URI uri, String fragment) {
+
+    private static URI createURI(final URI uri, final String fragment) {
         if (Strings.isNullOrEmpty(fragment)) {
             return uri;
         }
