c8a11934b159c12ffe4e0dfd5f67f4ba11773a84
Michael Langhammer <langhammer@kit.edu>
2016-02-26 10:40:42 +0000

    Share project "org.somox.util" into "https://svnserver.informatik.kit.edu/i43/svn/code/SoMoX"
    git-svn-id: https://svnserver.informatik.kit.edu/i43/svn/code/SoMoX/Core/branches/SoMoXWithJaMoPP@31219 769cc9a0-fb0c-0410-8c2f-a2305f0e6268

---
+++ org.somox.util/src/org/somox/util/Seff2JavaCreatorUtil.java
@@ -0,0 +1,30 @@
+package org.somox.util;
+
+import org.palladiosimulator.pcm.repository.BasicComponent;
+import org.palladiosimulator.pcm.repository.ProvidedRole;
+import org.palladiosimulator.pcm.repository.Repository;
+import org.palladiosimulator.pcm.repository.RepositoryComponent;
+import org.somox.analyzer.AnalysisResult;
+import org.somox.analyzer.simplemodelanalyzer.builder.Seff2JavaASTBuilder;
+
+public final class Seff2JavaCreatorUtil {
+
+    private Seff2JavaCreatorUtil() {
+
+    }
+
+    public static void executeSeff2JavaAST(final AnalysisResult analysisResult,
+            final org.somox.kdmhelper.metamodeladdition.Root root) {
+        final Seff2JavaASTBuilder seff2JavaASTBuilder = new Seff2JavaASTBuilder(root, null, analysisResult);
+        final Repository repo = analysisResult.getInternalArchitectureModel();
+        for (final RepositoryComponent repoComponent : repo.getComponents__Repository()) {
+            if (repoComponent instanceof BasicComponent) {
+                final BasicComponent bc = (BasicComponent) repoComponent;
+                for (final ProvidedRole providedRole : bc.getProvidedRoles_InterfaceProvidingEntity()) {
+                    seff2JavaASTBuilder.addSeffsToPrimitiveComponent(bc, providedRole);
+                }
+            }
+        }
+    }
+
+}
