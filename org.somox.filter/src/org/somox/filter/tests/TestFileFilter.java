792d890371bd5356fdd07c32759f276bf85b5c6e
Joshua Gleitze <joshua.gleitze@student.kit.edu>
2016-09-18 11:42:58 +0000

    Added a cache and null handling to TestFileFilter
    git-svn-id: https://svnserver.informatik.kit.edu/i43/svn/code/SoMoX/Core/branches/SoMoXWithJaMoPP@33713 769cc9a0-fb0c-0410-8c2f-a2305f0e6268

--- org.somox.filter/src/org/somox/filter/tests/TestFileFilter.java
+++ org.somox.filter/src/org/somox/filter/tests/TestFileFilter.java
@@ -2,6 +2,8 @@ package org.somox.filter.tests;
 
 import java.io.IOException;
 import java.nio.file.Path;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Optional;
 
 import org.apache.log4j.Logger;
@@ -10,24 +12,44 @@ import org.somox.filter.BaseFilter;
 import org.somox.filter.BlacklistFilter;
 
 public class TestFileFilter extends BaseFilter<ConcreteClassifier> {
-    
+
     private static final Logger logger = Logger.getLogger(BlacklistFilter.class);
 
     /**
-     * The detectors that are used to determine whether a file is a test file. Because
-     * ordering affects performance, this is not a set.
+     * Caches the results of {@link #isTestFile(ConcreteClassifier)}.
+     */
+    private final Map<ConcreteClassifier, Boolean> cache = new HashMap<>();
+
+    /**
+     * The detectors that are used to determine whether a file is a test file. Because ordering
+     * affects performance, this is not a set.
      */
     private final TestDetector[] detectors = new TestDetector[] { new MavenProjectStructureTestFileDetector(),
             new JUnitAnnotationTestDetector() };
 
     @Override
     public boolean passes(ConcreteClassifier object) {
-        final Path compilationUnitPath = EmfResource.getPath(object.getContainingCompilationUnit().eResource());
-        final boolean isTestFile = this.isTestFile(compilationUnitPath);
-        if (isTestFile) {
-            logger.debug("Excluded \"" + compilationUnitPath + "\" because it’s a test file.");
+        return !cache.computeIfAbsent(object, this::isTestFile);
+    }
+
+    /**
+     * Determines whether the provided {@code classifier} is a test file, using the
+     * {@link #detectors}. The method’s behaviour is equivalent to {@link #isTestFile(Path)}.
+     *
+     * @param classifier
+     *            A JaMoPP concrete classifier.
+     * @return Whether the provided {@code classifier} is a test file.
+     */
+    private boolean isTestFile(final ConcreteClassifier classifier) {
+        final Path compilationUnitPath = EmfResource.getPath(classifier.getContainingCompilationUnit().eResource());
+        if (compilationUnitPath != null) {
+            final boolean isTestFile = this.isTestFile(compilationUnitPath);
+            if (isTestFile) {
+                logger.debug("Excluded \"" + compilationUnitPath + "\" because it’s a test file.");
+            }
+            return !isTestFile;
         }
-        return !isTestFile;
+        return false;
     }
 
     /**
