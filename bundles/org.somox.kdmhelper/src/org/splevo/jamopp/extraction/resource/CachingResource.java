d0f40fa2874b96ce5de1208c481e266b5ccc0c35
Kirschner, Yves (IPD) <yves.kirschner@kit.edu>
2020-11-20 15:19:47 +0100

    Checkout the bundle org.somox.kdmhelper from the branch migrateBuild-newjamopp.
    To migrate the build, we iteratively migrate the bundles in the order of their compile time dependencies.

---
+++ bundles/org.somox.kdmhelper/src/org/splevo/jamopp/extraction/resource/CachingResource.java
@@ -0,0 +1,13 @@
+package org.splevo.jamopp.extraction.resource;
+
+/**
+ * Base interface for caching resources.
+ */
+public interface CachingResource {
+
+    /**
+     * Disables the caching mechanism for this resource.
+     */
+    void disableCaching();
+
+}
