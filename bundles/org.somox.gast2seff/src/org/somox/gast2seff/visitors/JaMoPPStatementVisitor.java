84481ab87a19520a25abe60c6dfb060562318380
mazkatli <30339210+mazkatli@users.noreply.github.com>
2021-01-30 08:21:58 +0100

    Revert "The compilation errors in org.somox.gast2seff have been temporarily fixed." after migrating SimplemodelAnalyzer
    This reverts commit aa99b6821bc26ddf6b1385152d3972cb47487074.

--- bundles/org.somox.gast2seff/src/org/somox/gast2seff/visitors/JaMoPPStatementVisitor.java
+++ bundles/org.somox.gast2seff/src/org/somox/gast2seff/visitors/JaMoPPStatementVisitor.java
@@ -399,7 +399,7 @@ public class JaMoPPStatementVisitor extends AbstractJaMoPPStatementVisitor {
     private void createInternalCallAction(final ResourceDemandingInternalBehaviour resourceDemandingBehavior,
             final Statement callStatement) {
         final InternalCallAction internalCallAction = SeffFactory.eINSTANCE.createInternalCallAction();
-        // internalCallAction.setCalledResourceDemandingInternalBehaviour(resourceDemandingBehavior);
+        internalCallAction.setCalledResourceDemandingInternalBehaviour(resourceDemandingBehavior);
         internalCallAction.setEntityName(this.positionToString(callStatement));
         if (null != callStatement) {
             this.createAbstracActionClassMethodLink(internalCallAction, callStatement);
