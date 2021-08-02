f289fc249796e70b0e37f8924aa60604c87ffb71
Yves <10582363+YvesKirschner@users.noreply.github.com>
2021-08-02 16:31:06 +0200

    Delete bundles/org.somox.analyzer.simplemodelanalyzer directory

--- bundles/org.somox.analyzer.simplemodelanalyzer/src/org/somox/analyzer/simplemodelanalyzer/builder/util/InstanceComponentTuple.java
+++
@@ -1,9 +0,0 @@
-package org.somox.analyzer.simplemodelanalyzer.builder.util;
-
-import org.palladiosimulator.pcm.core.composition.AssemblyContext;
-import org.palladiosimulator.pcm.repository.BasicComponent;
-
-public class InstanceComponentTuple {
-    public BasicComponent basicComponent;
-    public AssemblyContext assemblyContext;
-}
