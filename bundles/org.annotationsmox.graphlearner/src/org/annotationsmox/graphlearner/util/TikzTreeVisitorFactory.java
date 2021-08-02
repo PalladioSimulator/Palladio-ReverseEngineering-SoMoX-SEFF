569f3fd1cc1c5a9f910e4f8d3114b6288b5a83c6
Yves <10582363+YvesKirschner@users.noreply.github.com>
2021-08-02 16:30:43 +0200

    Delete bundles/org.annotationsmox.graphlearner directory

--- bundles/org.annotationsmox.graphlearner/src/org/annotationsmox/graphlearner/util/TikzTreeVisitorFactory.java
+++
@@ -1,9 +0,0 @@
-package org.annotationsmox.graphlearner.util;
-
-import org.annotationsmox.graphlearner.visitor.TikZTreeVisitor;
-
-public interface TikzTreeVisitorFactory {
-
-    TikZTreeVisitor create(); 
-    
-}
