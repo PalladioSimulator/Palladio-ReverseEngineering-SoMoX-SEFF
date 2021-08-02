5142d0c8b510d93869f81bc4ad17047762f132f8
Yves <10582363+YvesKirschner@users.noreply.github.com>
2021-08-02 16:30:53 +0200

    Delete bundles/org.annotationsmox directory

--- bundles/org.annotationsmox/src/org/annotationsmox/workflow/AnnotationsMoxWorkflowHooks.java
+++
@@ -1,9 +0,0 @@
-package org.annotationsmox.workflow;
-
-public class AnnotationsMoxWorkflowHooks {
-
-    public static final String PRE_SAVE_MODELS = "pre_save_models";
-
-    public static final String POST_SAVE_MODELS = "post_save_models";
-
-}
