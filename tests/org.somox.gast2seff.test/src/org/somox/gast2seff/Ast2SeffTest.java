b7d6aaf9fcb2009a56a6ad1a214468642748375a
Moritz Gstuer <moritz@gstuer.com>
2022-05-13 21:05:36 +0200

    Add placeholder jupiter test for ast2SeffJob

---
+++ tests/org.somox.gast2seff.test/src/org/somox/gast2seff/Ast2SeffTest.java
@@ -0,0 +1,24 @@
+package org.somox.gast2seff;
+
+import org.eclipse.core.runtime.NullProgressMonitor;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.somox.gast2seff.jobs.Ast2Seff;
+
+import de.uka.ipd.sdq.workflow.blackboard.Blackboard;
+import de.uka.ipd.sdq.workflow.jobs.JobFailedException;
+import de.uka.ipd.sdq.workflow.jobs.UserCanceledException;
+
+public class Ast2SeffTest {
+    @Disabled
+    @Test
+    public void testJobAst2Seff() throws JobFailedException, UserCanceledException {
+        Ast2Seff ast2SeffJob = new Ast2Seff();
+        // TODO Fill blackboard with information (like root compilation units) for Ast2Seff Job
+        Blackboard<Object> blackboard = new Blackboard<>();
+        ast2SeffJob.setBlackboard(blackboard);
+        NullProgressMonitor progressMonitor = new NullProgressMonitor();
+        ast2SeffJob.execute(progressMonitor);
+        // TODO Formulate assertions for blackboard content (= results of execution)
+    }
+}
